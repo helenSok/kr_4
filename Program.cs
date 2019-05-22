3aef989efd8e889597256c2c8aa22068e6fb31a3
Helen <golubeva.sok@mail.ru>
2019-05-22 23:23:37 +0200

    Add files

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ namespace kr_4
     {
         static void Main(string[] args)
         {
+            string x;
             variant_2 tt = new variant_2();            
-            Console.WriteLine("Hello World!" + tt.test());            
+            Console.WriteLine("Hello World!" + tt.test());   
+            x = Console.ReadLine();         
+            Console.WriteLine("Hello World!" + tt.test());
+            Console.WriteLine(x);
+            Console.ReadKey();
         }
     }
 }
--- variant_1.cs
+++ variant_1.cs
@@ -4,6 +4,10 @@ namespace kr_4
 {
     class variant_1
     {
+        public void test2()
+        {
+            Console.WriteLine("Hey Bro");
+        }
         public string test()
         {
             return " Bro1";
